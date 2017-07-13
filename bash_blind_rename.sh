c3e01556015dae36a7938571980e3b6ae22d2fea
Enrique Toledo M <ToledoEM@users.noreply.github.com>
2017-07-13 12:51:48 +0200

    Add Analysis file

--- bash_blind_rename.sh
+++ bash_blind_rename.sh
@@ -6,6 +6,7 @@
 # Control if folder have been already randomized by existence of name_dictionary.csv
 # Check that folder it is in fact a folder and do not goes in subfolders
 # Reduce risk of filename collisions with shasum filename, cut in half
+# Create file Analysis_file.csv with only new names to be used for manual analysis
 echo
 echo Drop Folder into this windows
 echo
@@ -41,9 +42,9 @@ else
                  fi
             NEWNAME=$(echo $file | shasum | tr '[a-z]' '[A-Z]' |tr -dc 'A-Z0-9' | cut -c 1-20 )
             ext=${file##*.}
-            mv  $1/$file $1/$NEWNAME.$ext
             echo "$file,$NEWNAME.$ext" >> $1/name_dictionary.csv
-        
+            mv  $1/$file $1/$NEWNAME.$ext
+            echo "$NEWNAME.$ext" >> $1/Analysis_file.csv
         done
 fi
 fi
